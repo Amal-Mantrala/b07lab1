d25e353810ab97cda044ddd48c38a8f4765aa88d
Amal Mantrala <amaldutt.mantrala@gmail.com>
2025-10-01 23:59:46 -0400

    modified Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,28 +1,57 @@
 public class Polynomial {
     private double[] coefficients;
+    private int[] exponents;
     public Polynomial() {
         this.coefficients = new double[]{0};
+	this.exponents = new int []{0};
     }
-    public Polynomial(double[] coefficients) {
+    public Polynomial(double[] coefficients, int []exponents) {
         this.coefficients = new double[coefficients.length];
+	this.exponents = new int[exponents.length];
+	if(exponents.length != coefficients.length) { 
+	    return;	
+	}
         for (int i = 0; i < coefficients.length; i++) {
             this.coefficients[i] = coefficients[i];
+	    this.exponents[i] = exponents[i];
         }
     }
     public Polynomial add(Polynomial other) {
-        int maxLength = Math.max(this.coefficients.length, other.coefficients.length);
-        double[] result = new double[maxLength];
-        for (int i = 0; i < maxLength; i++) {
-            double a = (i < this.coefficients.length) ? this.coefficients[i] : 0;
-            double b = (i < other.coefficients.length) ? other.coefficients[i] : 0;
-            result[i] = a + b;
+        Map<Integer, Double> terms = new HashMap<>();
+        for (int i = 0; i < coefficients.length; i++) {
+            terms.put(exponents[i], terms.getOrDefault(exponents[i], 0.0) + coefficients[i]);
         }
-        return new Polynomial(result);
+        for (int i = 0; i < other.coefficients.length; i++) {
+            terms.put(other.exponents[i], terms.getOrDefault(other.exponents[i], 0.0) + other.coefficients[i]);
+        }
+        return fromMap(terms);
     }
+    public Polynomial multiply(Polynomial other) {
+        Map<Integer, Double> terms = new HashMap<>();
+        for (int i = 0; i < coefficients.length; i++) {
+            for (int j = 0; j < other.coefficients.length; j++) {
+                int e = exponents[i] + other.exponents[j];
+                double c = coefficients[i] * other.coefficients[j];
+                terms.put(e, terms.getOrDefault(exp, 0.0) + c);
+            }
+        }
+        return fromMap(terms);
+    }
+    private Polynomial fromMap(Map<Integer, Double> terms) {
+    	double[] c = new double[terms.size()];
+    	int[] e = new int[terms.size()];
+    	int k = 0;
+    	for (int h : terms.keySet()) {
+    	    c[k] = terms.get(h);
+   	    e[k] = h;
+            k++;
+    	}
+    	return new Polynomial(c, e);
+    }	
     public double evaluate(double x) {
         double result = 0;
         for (int i = 0; i < coefficients.length; i++) {
-            result += coefficients[i] * Math.pow(x, i);
+            result += coefficients[i] * Math.pow(x, exponents[i]);
         }
         return result;
     }
